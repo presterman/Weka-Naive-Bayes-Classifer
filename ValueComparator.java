76ed466e99bf09ebc0c9db34ae0259d972b061ff
presterman <presterman@gmail.com>
2016-08-12 15:15:46 -0700

    Create ValueComparator.java

---
+++ ValueComparator.java
@@ -0,0 +1,23 @@
+package kbPredict;
+import java.util.Comparator;
+import java.util.Map;
+ 
+public class ValueComparator implements Comparator {
+ 
+  Map map;
+ 
+  public ValueComparator(Map map){
+    this.map = map;
+  }
+  public int compare(Object keyA, Object keyB){
+ 
+    Comparable valueA = (Comparable) map.get(keyA);
+    Comparable valueB = (Comparable) map.get(keyB);
+ 
+  //  System.out.println(valueA +" - "+valueB);
+ 
+  //  return valueA.compareTo(valueB);
+    return valueB.compareTo(valueA);
+ 
+  }
+}
